9006fc146d714d6edc33e5d7a70beb354e8194b4
sanginchun <sanginchun91@gmail.com>
2025-06-08 14:50:42 +0900

    fix: array에서 중복 없이 랜덤 선택하도록 수정

--- src/utils/pickRandomFromArray.ts
+++ src/utils/pickRandomFromArray.ts
@@ -1,9 +1,13 @@
 function pickRandomFromArray<T>(array: T[], count: number): T[] {
   const result: T[] = [];
+  const availableIndices = array.map((_, index) => index);
 
   for (let i = 0; i < count; i++) {
-    const randomIndex = Math.floor(Math.random() * array.length);
-    result.push(array[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * availableIndices.length);
+    const selectedIndex = availableIndices[randomIndex];
+
+    result.push(array[selectedIndex]);
+    availableIndices.splice(randomIndex, 1);
   }
 
   return result;
