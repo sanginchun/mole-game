b0f5f3c259e709221139dae2d06258d98556c8f6
sanginchun <sanginchun91@gmail.com>
2025-06-08 14:43:46 +0900

    feat: 게임 플레이 화면 연결

--- src/screens/Play.tsx
+++ src/screens/Play.tsx
@@ -1,5 +1,55 @@
+import useMoleGame from "@/hooks/useMoleGame";
+import useGameSettingStore from "@/store/game-setting";
+
 const Play = () => {
-  return <main>Play</main>;
+  const settings = useGameSettingStore((state) => state.settings);
+  const { startGame, pauseGame, resumeGame, restartGame, gameState, hitHole } =
+    useMoleGame({
+      holes: settings.rows * settings.cols,
+      moles: settings.moles,
+    });
+
+  return (
+    <main>
+      <div>{gameState.score}</div>
+      <div>{gameState.timeRemaining}</div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${settings.cols}, 1fr)`,
+          gridTemplateRows: `repeat(${settings.rows}, 1fr)`,
+          gap: "1rem",
+          height: "80%",
+        }}
+      >
+        {gameState.holeStatus.map((v, i) => (
+          <div
+            key={i}
+            style={{
+              color: "white",
+              backgroundColor:
+                v === null ? "lightyellow" : v.isHit ? "red" : "brown",
+            }}
+            onClick={() => hitHole(i)}
+          >
+            {v === null ? "" : `${v.isHit ? "hit !!!" : ""}`}
+          </div>
+        ))}
+      </div>
+      <button onClick={startGame} disabled={gameState.status !== "IDLE"}>
+        Start
+      </button>
+      <button onClick={pauseGame} disabled={gameState.status !== "PLAYING"}>
+        Pause
+      </button>
+      <button onClick={resumeGame} disabled={gameState.status !== "PAUSED"}>
+        Resume
+      </button>
+      <button onClick={restartGame} disabled={gameState.status !== "PAUSED"}>
+        Restart
+      </button>
+    </main>
+  );
 };
 
 export default Play;
