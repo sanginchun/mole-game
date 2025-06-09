e8e8cc3ef74f32b33d0d3707411ad2c9282c385f
sanginchun <sanginchun91@gmail.com>
2025-06-09 22:27:54 +0900

    feat: 순위 화면

--- src/screens/Scoreboard.tsx
+++ src/screens/Scoreboard.tsx
@@ -1,5 +1,32 @@
+import useGameResultStore from "@/store/game-result";
+import { Link } from "react-router";
+
 const Scoreboard = () => {
-  return <main>Scoreboard</main>;
+  const topScores = useGameResultStore((state) => state.topScores);
+
+  // TODO: highlight
+  // const currentResult = useGameResultStore(state => state.currentResult)
+
+  return (
+    <main>
+      <h1>순위</h1>
+      <ol>
+        {topScores.map((result) => (
+          <li key={result.playedAt}>
+            {new Date(result.playedAt).toISOString()} - {result.score}점
+          </li>
+        ))}
+      </ol>
+      <div>
+        <Link to="/play">
+          <button>다시하기</button>
+        </Link>
+        <Link to="/">
+          <button>처음으로</button>
+        </Link>
+      </div>
+    </main>
+  );
 };
 
 export default Scoreboard;
