5940b8a461a0f881bf9c404cf4948c299a44ea87
sanginchun <sanginchun91@gmail.com>
2025-06-09 22:19:04 +0900

    feat: 게임 설정에도 persist 적용

--- src/store/game-setting.ts
+++ src/store/game-setting.ts
@@ -1,5 +1,8 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import gameSettingConfig from "@/configs/game-setting";
+
+const STORAGE_KEY = "game-setting";
 
 interface Settings {
   rows: number;
@@ -12,27 +15,32 @@ interface GameSettingState {
   configure: (s: Partial<Settings>) => void;
 }
 
-const useGameSettingStore = create<GameSettingState>()((set) => ({
-  settings: {
-    rows: gameSettingConfig.AVAIL_ROWS[0],
-    cols: gameSettingConfig.AVAIL_COLS[0],
-    moles: gameSettingConfig.MIN_MOLES,
-  },
-  configure: (settings) =>
-    set((state) => {
-      const nextSettings = { ...state.settings, ...settings };
+const useGameSettingStore = create<GameSettingState>()(
+  persist(
+    (set) => ({
+      settings: {
+        rows: gameSettingConfig.AVAIL_ROWS[0],
+        cols: gameSettingConfig.AVAIL_COLS[0],
+        moles: gameSettingConfig.MIN_MOLES,
+      },
+      configure: (settings) =>
+        set((state) => {
+          const nextSettings = { ...state.settings, ...settings };
 
-      if (settings.cols || settings.rows) {
-        const { rows, cols, moles } = nextSettings;
-        const maxMoles = gameSettingConfig.getMaxMoles(rows, cols);
+          if (settings.cols || settings.rows) {
+            const { rows, cols, moles } = nextSettings;
+            const maxMoles = gameSettingConfig.getMaxMoles(rows, cols);
 
-        if (moles > maxMoles) {
-          nextSettings.moles = maxMoles;
-        }
-      }
+            if (moles > maxMoles) {
+              nextSettings.moles = maxMoles;
+            }
+          }
 
-      return { settings: nextSettings };
+          return { settings: nextSettings };
+        }),
     }),
-}));
+    { name: STORAGE_KEY }
+  )
+);
 
 export default useGameSettingStore;
